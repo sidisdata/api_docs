133390ec3868b86b963e6d3ae9b4c35b94b93efa
Tyson Cardelli <tcardelli@sidis.ai>
2026-01-20 19:21:21 +0000

    Fix baseUrl to match repository name api_docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,12 @@ const config = {
   url: 'https://sidisdata.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/sidis-docs/',
+  baseUrl: '/api_docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'sidisdata', // Usually your GitHub org/user name.
-  projectName: 'sidis-docs', // Usually your repo name.
+  projectName: 'api_docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
