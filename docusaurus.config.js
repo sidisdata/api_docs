745672e66609c196a80adac5539a114e164a12d4
Tyson Cardelli <tcardelli@sidis.ai>
2026-03-24 17:31:08 +0000

    Update URL configuration for GitHub Pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://sidisdata.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/api_docs/',
